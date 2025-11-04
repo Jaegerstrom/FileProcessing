c7c959a79d9093eb3255f32adafe206a0f5dcff3
Jaegerstrom <sleeperhyper@gmail.com>
2025-11-04 18:21:25 +0800

    added try and more messagebox

--- File Processing/Form1.vb
+++ File Processing/Form1.vb
@@ -3,14 +3,21 @@
 Public Class Form1
 
     Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim filePath As String = "sample.txt"
+        Try
 
-        Using writer As New StreamWriter(filePath)
-            writer.WriteLine("Hello, this is a test file!")
-            writer.WriteLine("Second line of text.")
-        End Using
+            Dim filePath As String = "sample.txt"
 
-        MessageBox.Show("Data written successfully.", "Success")
+            Using writer As New StreamWriter(filePath)
+                writer.WriteLine("Hello, this is a test file!")
+                writer.WriteLine("Second line of text.")
+            End Using
+
+            MessageBox.Show("Data written successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
     End Sub
 
 End Class

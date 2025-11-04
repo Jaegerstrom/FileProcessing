3bb4a701b486c26ae6182ead12ad6a5a6dcc91d0
Jaegerstrom <sleeperhyper@gmail.com>
2025-11-04 18:53:25 +0800

    added listbox and new button (read per line)

--- File Processing/Form1.Designer.vb
+++ File Processing/Form1.Designer.vb
@@ -24,11 +24,13 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.ButtonRead = New System.Windows.Forms.Button()
+        Me.ButtonReadPerLine = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(303, 189)
+        Me.Button1.Location = New System.Drawing.Point(148, 189)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(190, 82)
         Me.Button1.TabIndex = 0
@@ -37,18 +39,37 @@ Partial Class Form1
         '
         'ButtonRead
         '
-        Me.ButtonRead.Location = New System.Drawing.Point(303, 89)
+        Me.ButtonRead.Location = New System.Drawing.Point(148, 89)
         Me.ButtonRead.Name = "ButtonRead"
         Me.ButtonRead.Size = New System.Drawing.Size(190, 82)
         Me.ButtonRead.TabIndex = 1
         Me.ButtonRead.Text = "Read"
         Me.ButtonRead.UseVisualStyleBackColor = True
         '
+        'ButtonReadPerLine
+        '
+        Me.ButtonReadPerLine.Location = New System.Drawing.Point(274, 324)
+        Me.ButtonReadPerLine.Name = "ButtonReadPerLine"
+        Me.ButtonReadPerLine.Size = New System.Drawing.Size(190, 82)
+        Me.ButtonReadPerLine.TabIndex = 2
+        Me.ButtonReadPerLine.Text = "Read Per Line"
+        Me.ButtonReadPerLine.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(148, 311)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 3
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ButtonReadPerLine)
         Me.Controls.Add(Me.ButtonRead)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -59,4 +80,6 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents ButtonRead As Button
+    Friend WithEvents ButtonReadPerLine As Button
+    Friend WithEvents ListBox1 As ListBox
 End Class
